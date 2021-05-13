6b700b92b83d7d5d3a065b38d625d451f68d4de2
codestance <skrzynska.konstancja@gmail.com>
2021-05-13 15:52:24 +0200

    Add RestaurantScreen and RestaurantList

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="app">Hello, world.</div>
+  <RestaurantScreen />
 </template>
 
 <script>
+import RestaurantScreen from '@components/RestaurantScreen';
 export default {
   name: 'App',
+  components: {RestaurantScreen},
 };
 </script>
